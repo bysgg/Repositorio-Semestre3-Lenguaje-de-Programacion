a80d8bcb2801ac727f8776154a52c411ee893fb7
SEBASTIAN GUTIERREZ GUAYACUNDO <sgutierrez78@unisalle.edu.co>
2026-03-21 08:11:03 -0500

    Definir modelos base y pagos de acuerdo a Principios SOLID

---
+++ ACT 3 y 6 - Python/Ejercicio_1/modelos.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class Producto:
+    def __init__(self, codigo: str, descripcion: str, precio: float, cantidad: int):
+        self.codigo = codigo
+        self.descripcion = descripcion
+        self.precio = precio
+        self.cantidad = cantidad
+
+class MetodoPago(ABC):
+    @abstractmethod
+    def procesar(self, monto: float) -> None:
+        pass
